ddda54f0ba65343bc6ec0bd76c911cb3ae5acb0b
jephthah <jephthahaansah@gmail.com>
2026-09-03 18:54:11 +0000

    created a simple api and added a data json file

--- api.js
+++ api.js
@@ -1,21 +1,31 @@
-//routing
+//building a simple api
+
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
-const overview = 'This is the OVERVIEW'
 
 //server 
+const data = fs.readFileSync(`${__dirname}/data.json`, 'utf-8');
+const productData = JSON.parse(data);
+
+
+
 const server = http.createServer((req,res) => {
 
     const pathName = req.url;
 
     if(pathName === '/' || pathName === '/overview') {
-        res.end(overview);
+        res.end('This is the OVERVIEW');
 
     } else if (pathName === '/product') {
         res.end('This is PRODUCT');
+      
+    } else if (pathName === '/api') {
+            res.writeHead(200, {'Content-type':'application/json'});
+            res.end(data);
+        } 
 
-    } else {
+    else {
         //adding an http status code and header
         res.writeHead(404 , {
             'Content-type' : 'text/html'
@@ -26,6 +36,6 @@ const server = http.createServer((req,res) => {
     
 });
 
-server.listen(5000, 'localhost', () => {
-    console.log('Server has started listening on port 5000...');
+server.listen(7000, 'localhost', () => {
+    console.log('Server has started listening on port 7000...');
 });
